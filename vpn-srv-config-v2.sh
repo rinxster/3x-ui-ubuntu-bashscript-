a04983fed29fbff3f43cfa90bae70f282802f7c4
rinxster <sark@bk.ru>
2024-05-11 13:23:06 +0300

    Update vpn-srv-config-v2.sh

--- vpn-srv-config-v2.sh
+++ vpn-srv-config-v2.sh
@@ -1,5 +1,4 @@
 # !/bin/bash
-
 echo '########################################'
 echo 'Change your default root password!'
 echo '########################################'
@@ -12,16 +11,16 @@ yes | sudo apt install mc && yes | apt install fail2ban && yes | apt install cur
 #configure and setup regular  automatic updates
 yes | sudo apt install unattended-upgrades && yes | sudo apt install update-notifier-common
 sudo echo -e "APT::Periodic::Update-Package-Lists \"1\";\nAPT::Periodic::Unattended-Upgrade \"1\";\n" > /etc/apt/apt.conf.d/20auto-upgrades
-sudo systemctl restart unattended-upgrades
 sudo systemctl enable unattended-upgrades
+sudo systemctl restart unattended-upgrades
 
 #sudo systemctl status unattended-upgrades
 
 #configure fail2ban
 sudo cp /etc/fail2ban/jail.{conf,local}
 sudo sed -i -e 's/bantime  = 10m/bantime  = 1d/g' /etc/fail2ban/jail.local
-sudo systemctl restart fail2ban
 sudo systemctl enable fail2ban
+sudo systemctl restart fail2ban
 #sudo systemctl status fail2ban
 
 #configure and start ufw
@@ -33,13 +32,10 @@ sudo ufw allow 22
 sudo ufw allow 2222
 sudo ufw allow 443
 sudo ufw allow 50444
-
-# disable ICMP from aninymizer detectors
+# disable ICMP in UFW from aninymizer detectors
 sudo sed -i '/ufw-before-input.*icmp/s/ACCEPT/DROP/g' /etc/ufw/before.rules 
-sudo ufw reload
-
 yes | sudo ufw enable
-#sudo ufw status verbose
+sudo ufw reload
 
 #disable ipv6 for security as not used and reboot the server
 sed -i '/GRUB_CMDLINE_LINUX/ s/"$/ ipv6.disable=1"/' /etc/default/grub
@@ -47,7 +43,8 @@ sudo update-grub
 
 # pause to see intermediate result.
 echo '########################################'
-read -n1 -s -r -p $'Everything is ready to change SSH port to 2222. Press space to continue...\n' key
+echo 'Everything is ready to change SSH port to 2222'
+#read -n1 -s -r -p $'Everything is ready to change SSH port to 2222. Press space to continue...\n' key
 echo '########################################'
 sudo sed -i -e 's/#Port 22/Port 2222/g' /etc/ssh/sshd_config && service sshd reload 
 
